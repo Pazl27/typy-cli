e63cdb47637d82ed3fef03d6cc4325f524f6aeb4
Paul <jaul.ponas@gmail.com>
2025-02-02 22:59:40 +0100

    small bug fix and new struct name

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,16 @@ use crossterm::{
 use std::io::stdout;
 use std::io::Write;
 
-struct Words<'a> {
+struct Game<'a> {
     list: Vec<&'a str>,
     player_position: i32,
     jump_position: i32,
     selected_word_index: i32,
 }
 
-impl<'a> Words<'a> {
+impl<'a> Game<'a> {
     fn new(list: Vec<&'a str>) -> Self {
-        Words {
+        Game {
             list,
             player_position: 0,
             jump_position: 0,
@@ -32,20 +32,20 @@ fn main() {
     let mut stdout = stdout();
 
     let word_list = "Hello World I love you";
-    let mut words = Words::new(word_list.split_whitespace().collect::<Vec<&str>>());
-    // let mut position = 0;
-    // let mut jump_position = 0;
-    // let mut word_selected = 0;
+    let mut game = Game::new(word_list.split_whitespace().collect::<Vec<&str>>());
 
     setup_terminal(&stdout);
     let (cols, rows) = crossterm::terminal::size().unwrap();
     let x = cols / 2 - (word_list.chars().count() / 2) as u16;
     let y = rows / 2;
 
-    print_words(x, y, &words.list, &stdout);
+    print_words(x, y, &game.list, &stdout);
     stdout.execute(MoveTo(x, y)).unwrap();
 
     loop {
+        if game.player_position == word_list.chars().count() as i32 {
+            break;
+        }
         if let Ok(Event::Key(KeyEvent {
             code, modifiers, ..
         })) = read()
@@ -55,36 +55,36 @@ fn main() {
             }
             if let KeyCode::Char(c) = code {
                 if c == ' ' {
-                    if words.selected_word_index == words.list.len() as i32 - 1 {
+                    if game.selected_word_index == game.list.len() as i32 - 1 {
                         break;
                     }
-                    if words.jump_position + 1 == words.player_position && words.jump_position != 0 {
+                    if game.jump_position + 1 == game.player_position && game.jump_position != 0 {
                         continue;
                     }
-                    words.jump_position = words.list
+                    game.jump_position = game.list
                         .iter()
-                        .take(words.selected_word_index as usize + 1)
+                        .take(game.selected_word_index as usize + 1)
                         .map(|word| word.chars().count() + 1)
                         .sum::<usize>() as i32
                         - 1;
-                    words.player_position = words.jump_position;
-                    stdout.execute(MoveTo(x + words.player_position as u16, y)).unwrap();
-                    words.selected_word_index += 1;
+                    game.player_position = game.jump_position;
+                    stdout.execute(MoveTo(x + game.player_position as u16, y)).unwrap();
+                    game.selected_word_index += 1;
                 }
-                if c == word_list.chars().nth(words.player_position as usize).unwrap() {
+                if c == word_list.chars().nth(game.player_position as usize).unwrap() {
                     stdout.execute(SetForegroundColor(Color::White)).unwrap();
-                    stdout.execute(MoveTo(x + words.player_position as u16, y)).unwrap();
-                    print!("{}", word_list.chars().nth(words.player_position as usize).unwrap());
+                    stdout.execute(MoveTo(x + game.player_position as u16, y)).unwrap();
+                    print!("{}", word_list.chars().nth(game.player_position as usize).unwrap());
                 } else {
                     stdout.execute(SetForegroundColor(Color::Red)).unwrap();
-                    stdout.execute(MoveTo(x + words.player_position as u16, y)).unwrap();
-                    print!("{}", word_list.chars().nth(words.player_position as usize).unwrap());
+                    stdout.execute(MoveTo(x + game.player_position as u16, y)).unwrap();
+                    print!("{}", word_list.chars().nth(game.player_position as usize).unwrap());
                 }
-                if word_list.chars().nth(words.player_position as usize).unwrap() == ' ' && c != ' ' {
-                    words.selected_word_index += 1;
+                if word_list.chars().nth(game.player_position as usize).unwrap() == ' ' && c != ' ' {
+                    game.selected_word_index += 1;
                 }
                 stdout.flush().unwrap();
-                words.player_position += 1;
+                game.player_position += 1;
             }
         }
     }
