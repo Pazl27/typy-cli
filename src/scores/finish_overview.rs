9b52051f7aac5cc36e13e61942e96d2682bde775
Pazl27 <your@example.com>
2025-02-10 22:03:15 +0100

    better position of scores

--- src/scores/finish_overview.rs
+++ src/scores/finish_overview.rs
@@ -15,34 +15,33 @@ pub fn show_stats(
     theme: &ThemeColors
 ) -> Option<()> {
 
+    let (_, y) = utils::calc_size();
+
     stdout.execute(Clear(ClearType::All)).unwrap();
 
-    // Draw infos
-    stdout.execute(MoveTo(15, 16)).unwrap();
-    stdout
-        .execute(SetForegroundColor(theme.missing))
-        .unwrap();
-    print!("WPM");
-    stdout.execute(MoveTo(15, 17)).unwrap();
-    stdout
-        .execute(SetForegroundColor(theme.accent))
-        .unwrap();
+    stdout.execute(MoveTo(15, (y / 2) + 13)).unwrap();
+    stdout.execute(SetForegroundColor(theme.missing)).unwrap();
+    print!("WPM:");
+
+    stdout.execute(MoveTo(20, (y / 2) + 13)).unwrap();
+    stdout.execute(SetForegroundColor(theme.accent)).unwrap();
     print!("{:02}", stats.wpm() as i32);
-    stdout.execute(MoveTo(15, 20)).unwrap();
-    stdout
-        .execute(SetForegroundColor(theme.missing))
-        .unwrap();
-    print!("RAW");
-    stdout.execute(MoveTo(15, 21)).unwrap();
-    stdout
-        .execute(SetForegroundColor(theme.accent))
-        .unwrap();
+
+    stdout.execute(MoveTo(25, (y / 2) + 13)).unwrap();
+    stdout.execute(SetForegroundColor(theme.missing)).unwrap();
+    print!("RAW:");
+
+    stdout.execute(MoveTo(30, (y / 2) + 13)).unwrap();
+    stdout.execute(SetForegroundColor(theme.accent)).unwrap();
     print!("{:02}", stats.raw_wpm() as i32);
-    stdout.execute(MoveTo(37, 25)).unwrap();
-    stdout
-        .execute(SetForegroundColor(theme.accent))
-        .unwrap();
-    print!("ACCURACY: {:.2}%", stats.accuracy());
+
+    stdout.execute(MoveTo(35, (y / 2) + 13)).unwrap();
+    stdout.execute(SetForegroundColor(theme.missing)).unwrap();
+    print!("ACCURACY:");
+
+    stdout.execute(MoveTo(45, (y / 2) + 13)).unwrap();
+    stdout.execute(SetForegroundColor(theme.accent)).unwrap();
+    print!("{:.2}%", stats.accuracy());
 
     graph::draw_graph(stats.lps).unwrap();
 
