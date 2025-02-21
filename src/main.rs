4a9917e5abfed57d7f8d162081dd2c8ee4559ecc
Paul <jaul.ponas@gmail.com>
2025-02-21 18:01:49 +0100

    new clap version

--- src/main.rs
+++ src/main.rs
@@ -6,63 +6,51 @@ mod utils;
 mod word_provider;
 
 use anyhow::{Context, Result};
-use clap::{App, Arg};
 use mode::Mode;
+use clap::Parser;
+
+
+#[derive(Parser)]
+#[command(name = "typy")]
+#[command(version = "0.1.0")]
+#[command(author = "Pazl27")]
+#[command(about = "Monkeytype clone in the terminal for more information check: https://github.com/Pazl27/typy-cli")]
+#[command(long_about = None)]
+struct Cli {
+    #[arg(short = 't', long = "time", default_value = "10", help = "Duration of the game")]
+    time: u64,
+
+    #[arg(short = 's', long = "stats", help = "Display game stats")]
+    stats: bool,
+
+    #[arg(short = 'c', long = "config", help = "Create and open config file")]
+    config: bool,
+
+    #[arg(short = 'm', long = "mode", num_args = 1.., help = "Sets the mode of the game")]
+    mode: Vec<String>,
+}
+
 
 fn main() -> Result<()> {
-    let matches = App::new("Typy CLI")
-        .version("0.1.0")
-        .author("Pazl")
-        .about("Monkeytype clone in the terminal for more information check: https://github.com/Pazl27/typy-cli")
-        .arg(
-            Arg::new("duration")
-                .short('t')
-                .long("time")
-                .help("Sets the duration of the game")
-                .default_value("30")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::new("stats")
-                .short('s')
-                .long("stats")
-                .help("Shows the stats of the game"),
-        )
-        .arg(
-            Arg::new("config")
-                .short('c')
-                .long("config")
-                .help("Creates config file if it doesn't exist and opens it"),
-        )
-        .arg(
-            Arg::new("mode")
-                .short('m')
-                .long("mode")
-                .help("Sets the mode of the game")
-                .multiple_values(true)
-                .takes_value(true),
-        )
-        .get_matches();
 
-    let duration_str = matches
-        .value_of("duration")
-        .context("Failed to get duration value")?;
-    let duration: u64 = duration_str.parse().context("Invalid duration value")?;
+    let cli = Cli::parse();
+
+    let duration: u64 = cli.time;
 
     let theme = config::theme::ThemeColors::new();
 
-    if matches.is_present("config") {
+    if cli.config {
         utils::create_config()?;
         utils::open_config()?;
         return Ok(());
     }
 
-    if matches.is_present("stats") {
+    if cli.stats {
         println!("Stats");
         return Ok(());
     }
 
-    let mut mode_strs: Vec<&str> = matches.values_of("mode").unwrap_or_default().collect();
+    let mut mode_strs: Vec<&str> = cli.mode.iter().map(|s| s.as_str()).collect();
     mode_strs.is_empty().then(|| mode_strs.clear());
 
     let mode = Mode::from_str(mode_strs)
