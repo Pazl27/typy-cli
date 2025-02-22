8cabf80a540c9e27e615654e1e1beeef6f86f3bd
Paul <jaul.ponas@gmail.com>
2025-02-22 23:33:28 +0100

    save scores for tracking

---
+++ src/scores/progress/mod.rs
@@ -0,0 +1,78 @@
+use anyhow::{Context, Result};
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use serde_json::to_writer_pretty;
+use std::fs::{self, File};
+
+pub mod display;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Score {
+    timestamp: NaiveDateTime,
+    wpm: u32,
+    raw: u32,
+    accuracy: f32,
+}
+
+impl Score {
+    pub fn new(wpm: u32, raw: u32, accuracy: f32) -> Score {
+        Score {
+            timestamp: chrono::Local::now().naive_local(),
+            wpm,
+            raw,
+            accuracy,
+        }
+    }
+}
+
+pub fn save_score(score: Score) -> Result<()> {
+    let mut scores = get_scores()?;
+    scores.push(score);
+
+    if scores.len() > 10 {
+        scores.sort_by(|a, b| b.timestamp.cmp(&a.timestamp));
+        cleanup_scores(&mut scores);
+    }
+
+    write_to_file(scores)?;
+
+    Ok(())
+}
+
+fn cleanup_scores(scores: &mut Vec<Score>) {
+    scores.truncate(10);
+}
+
+fn get_scores() -> Result<Vec<Score>> {
+    let mut path = dirs::home_dir().context("Failed to get home directory")?;
+    path.push(".local/share/typy/scores.json");
+
+    if !path.exists() {
+        if let Some(parent) = path.parent() {
+            fs::create_dir_all(parent).context("Failed to create directories")?;
+        }
+        File::create(&path).context("Failed to create scores.json file")?;
+    }
+
+    let file = File::open(&path).context("Failed to open scores.json file")?;
+    let scores: Vec<Score> = match serde_json::from_reader(file) {
+        Ok(scores) => scores,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => return Err(e).context("Failed to read scores from file"),
+    };
+    Ok(scores)
+}
+
+fn write_to_file(scores: Vec<Score>) -> Result<()> {
+    let mut path = dirs::home_dir().context("Failed to get home directory")?;
+    path.push(".local/share/typy/scores.json");
+
+    if !path.exists() {
+        return Err(anyhow::anyhow!("File does not exist"));
+    }
+
+    let mut file = File::create(&path).context("Failed to truncate scores.json file")?;
+    to_writer_pretty(&mut file, &scores).context("Failed to write scores to file")?;
+
+    Ok(())
+}
