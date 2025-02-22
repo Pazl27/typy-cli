782661dab4a07d12c4e9a109516ec46a9d34d369
Paul <jaul.ponas@gmail.com>
2025-02-22 02:29:53 +0100

    new letters get displayed

--- src/terminal/keyboard.rs
+++ src/terminal/keyboard.rs
@@ -1,10 +1,10 @@
 use std::io::Write;
 
 use anyhow::{Context, Result};
-use crossterm::cursor::MoveTo;
 use crossterm::event::KeyCode;
-use crossterm::style::SetForegroundColor;
+use crossterm::style::{Attribute, SetForegroundColor};
 use crossterm::ExecutableCommand;
+use crossterm::{cursor::MoveTo, style::SetAttribute};
 
 use crate::{config::theme::ThemeColors, scores::stats::Stats};
 
@@ -37,7 +37,11 @@ pub fn handle_input(
         if game.player.position_x
             < game.get_word_string(game.player.position_y).chars().count() as i32
         {
-            handle_chars(game, stats, theme, stdout, c, x, y)?;
+            match handle_chars(game, stats, theme, stdout, c, x, y)? {
+                InputAction::Continue => return Ok(InputAction::Continue),
+                InputAction::Break => return Ok(InputAction::Break),
+                InputAction::None => {}
+            }
         }
         stdout.flush().context("Failed to flush stdout")?;
     }
@@ -48,7 +52,7 @@ fn handle_space(game: &mut Game, stdout: &std::io::Stdout, x: u16, y: u16) -> Re
     if let InputAction::Continue = handle_space_at_start(game)? {
         return Ok(InputAction::Continue);
     }
-    
+
     if let InputAction::Continue = handle_start_of_line(game)? {
         return Ok(InputAction::Continue);
     }
@@ -154,7 +158,7 @@ fn handle_chars(
     c: char,
     x: u16,
     y: u16,
-) -> Result<()> {
+) -> Result<InputAction> {
     let expected_char = game
         .get_word_string(game.player.position_y)
         .chars()
@@ -163,13 +167,23 @@ fn handle_chars(
 
     if c == expected_char {
         handle_correct_char(game, theme, stdout, c, x, y)?;
+    } else if game
+        .get_word_string(game.player.position_y)
+        .chars()
+        .nth(game.player.position_x as usize)
+        .context("Failed to get character from word")?
+        == ' '
+    {
+        if let InputAction::Continue = add_incorrect_char(game, theme, stdout, c, x, y)? {
+            return Ok(InputAction::Continue)
+        }
     } else {
         handle_incorrect_char(game, theme, stdout, expected_char, x, y)?;
     }
 
     update_game_state(game, stats, c)?;
 
-    Ok(())
+    Ok(InputAction::None)
 }
 
 fn handle_correct_char(
@@ -212,6 +226,42 @@ fn handle_incorrect_char(
         .context("Failed to move cursor")?;
     print!("{}", c);
     Ok(())
+}
+
+fn add_incorrect_char(
+    game: &mut Game,
+    theme: &ThemeColors,
+    mut stdout: &std::io::Stdout,
+    c: char,
+    x: u16,
+    y: u16,
+) -> Result<InputAction> {
+    let position_x = game.player.position_x;
+    let words = game.get_word_string(game.player.position_y);
+
+    if words.len() >= 90 {
+        return Ok(InputAction::Continue)
+    }
+
+    let before = words.chars().take(position_x as usize).collect::<String>();
+    let after = words.chars().skip(position_x as usize).collect::<String>();
+
+    stdout.execute(MoveTo(
+        game.player.position_x as u16 + x,
+        game.player.position_y as u16 + y,
+    ))?;
+
+    stdout.execute(SetForegroundColor(theme.error))?;
+    stdout.execute(SetAttribute(Attribute::Underlined))?;
+    print!("{}", c);
+    stdout.execute(SetForegroundColor(theme.missing))?;
+    stdout.execute(SetAttribute(Attribute::Reset))?; // Reset attributes to default
+    print!("{}", after);
+
+    let new_line = format!("{}{}{}", before, c, after);
+    game.list[game.player.position_y as usize] =
+        new_line.split_whitespace().map(String::from).collect();
+    Ok(InputAction::None)
 }
 
 fn update_game_state(game: &mut Game, stats: &mut Stats, c: char) -> Result<()> {
