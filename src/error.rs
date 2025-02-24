8fa47526f6cc4efe421f359a79a9052256ddad4f
Paul <jaul.ponas@gmail.com>
2025-02-24 14:54:20 +0100

    custom error

---
+++ src/error.rs
@@ -0,0 +1,31 @@
+use derive_more::From;
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug, From)]
+pub enum Error {
+    Custom(String),
+    Io(std::io::Error),
+    ParseInt(std::num::ParseIntError),
+    // Add more variants as needed
+}
+
+impl Error {
+    pub fn custom(val: impl std::fmt::Display) -> Self {
+        Self::Custom(val.to_string())
+    }
+}
+
+impl From<&str> for Error {
+    fn from(val: &str) -> Self {
+        Self::Custom(val.to_string())
+    }
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
